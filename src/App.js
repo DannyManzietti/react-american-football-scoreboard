9435df23fdb213de0b023da58c4fb820e42d7b94
Danny Manzietti <danmanzietti@gmail.com>
2019-09-09 18:41:00 -0400

    met some stretch, working on others

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const[homeScore, setHomeScore] = useState(0);
 const[awayScore, setAwayScore] = useState(0);
+const [quarter, setQuarter] = useState(0);
+const [down, setDown] = useState(0);
+const [yards,setYards] = useState(0);
 
 const handleHomeScore = points =>{
   setHomeScore(homeScore + points);
@@ -44,6 +47,8 @@ const handleAwayScore = points =>{
           <button className="awayButtons__touchdown" onClick = {() => handleAwayScore(7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick = {() => handleAwayScore(3)}>Away Field Goal</button>
         </div>
+        
+        
       </section>
     </div>
   );
